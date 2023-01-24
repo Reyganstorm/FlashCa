715e3d35e0ed74889678aad4d9f61b807b2efc3c
Reyganstorm <ruslan.shtybaev@gmail.com>
2023-01-24 18:57:58 +0300

    ADD: - methods to soloCategoryView

--- FlashCa/ViewControllers/SoloCategory/SoloCategoryView.swift
+++ FlashCa/ViewControllers/SoloCategory/SoloCategoryView.swift
@@ -9,4 +9,23 @@ import UIKit
 
 class SoloCategoryView: UIView {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addViews()
+        layoutViews()
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension SoloCategoryView {
+    func addViews() {}
+    
+    func layoutViews() {}
+    
+    func configure() {}
 }
