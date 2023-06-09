48388778f044b9c52daa3584a362c473688371a8
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2023-06-09 23:47:26 +0300

    INSTALL: - RealmSwift pod with SPM

--- FlashCa/Helpers/StorageManager.swift
+++ FlashCa/Helpers/StorageManager.swift
@@ -1,96 +1,97 @@
-////
-////  StorageManager.swift
-////  FlashCa
-////
-////  Created by Руслан Штыбаев on 19.01.2023.
-////
 //
-//import RealmSwift
+//  StorageManager.swift
+//  FlashCa
 //
-//class StorageManager {
-//    static let shared = StorageManager()
-//    
-//    // Open the local-only default realm
-//    let localRealm = try! Realm()
-//    
-//    
-//    private init() {}
-//    
-//    // MARK: - Category
-//    
-//    func save(_ categories: [Category]) {
-//        write {
-//            localRealm.add(categories)
-//        }
-//    }
-//    
-//    func save(_ category: Category) {
-//        write {
-//            localRealm.add(category)
-//        }
-//    }
-//    
-//    // MARK: - Card
-//    
-//    func save(_ card: Card, to category: Category) {
-//        write {
-//            category.cards.append(card)
-//        }
-//    }
-//    
-//    // MARK: - Delete
-//    
-//    func delete(_ category: Category) {
-//        write {
-//            localRealm.delete(category.cards)
-//            localRealm.delete(category)
-//        }
-//    }
-//    
-//    func delete(_ card: Card) {
-//        write {
-//            localRealm.delete(card)
-//        }
-//    }
+//  Created by Руслан Штыбаев on 19.01.2023.
 //
-//    
-//    // MARK: - Edit
-//    
-//    func edit(_ category: Category, newValue: String) {
+
+import RealmSwift
+import Foundation
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    // Open the local-only default realm
+    let localRealm = try! Realm()
+    
+    
+    private init() {}
+    
+    // MARK: - Category
+    
+    func save(_ categories: [Category]) {
+        write {
+            localRealm.add(categories)
+        }
+    }
+    
+    func save(_ category: Category) {
+        write {
+            localRealm.add(category)
+        }
+    }
+    
+    // MARK: - Card
+    
+    func save(_ card: Card, to category: Category) {
+        write {
+            category.cards.append(card)
+        }
+    }
+    
+    // MARK: - Delete
+    
+    func delete(_ category: Category) {
+        write {
+            localRealm.delete(category.cards)
+            localRealm.delete(category)
+        }
+    }
+    
+    func delete(_ card: Card) {
+        write {
+            localRealm.delete(card)
+        }
+    }
+
+    
+    // MARK: - Edit
+    
+    func edit(_ category: Category, newValue: String) {
+        write {
+            category.name = newValue
+        }
+    }
+    
+    func edit(_ card: Card, newCard: Card) {
+        write {
+            card.name = newCard.name
+            card.date = Date()
+            card.note = newCard.note
+        }
+    }
+    
+//    // MARK: - Done
+//    func done(_ taskList: TaskList) {
 //        write {
-//            category.name = newValue
+//            taskList.tasks.setValue(true, forKey: "isCompleted")
 //        }
 //    }
 //    
-//    func edit(_ card: Card, newCard: Card) {
+//    func done(_ task: Task) {
 //        write {
-//            card.name = newCard.name
-//            card.date = Date()
-//            card.note = newCard.note
+//            task.isCompleted.toggle()
 //        }
 //    }
-//    
-////    // MARK: - Done
-////    func done(_ taskList: TaskList) {
-////        write {
-////            taskList.tasks.setValue(true, forKey: "isCompleted")
-////        }
-////    }
-////    
-////    func done(_ task: Task) {
-////        write {
-////            task.isCompleted.toggle()
-////        }
-////    }
-//    
-//    
-//    private func write(completion: ()-> Void) {
-//        do {
-//            try localRealm.write {
-//                completion()
-//            }
-//        } catch let error {
-//            print(error)
-//        }
-//    }
-//}
+    
+    
+    private func write(completion: ()-> Void) {
+        do {
+            try localRealm.write {
+                completion()
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+}
--- FlashCa/Models/Category.swift
+++ FlashCa/Models/Category.swift
@@ -5,13 +5,13 @@
 //  Created by Руслан Штыбаев on 19.01.2023.
 //
 
-//import RealmSwift
-//
-//// Object - это тип данных фреймворка Realm
-//class Category: Object {
-//    
-//    @Persisted var name = ""
-//    @Persisted var date = Date()
-//    @Persisted var cards = List<Card>()
-//    
-//}
+import RealmSwift
+
+// Object - это тип данных фреймворка Realm
+class Category: Object {
+    
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var cards = List<Card>()
+    
+}
